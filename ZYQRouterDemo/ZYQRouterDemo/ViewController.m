a4908a7574de2698e6d4117cdc578eafcbb60fcd
zhaoyiqi <heroims@163.com>
2017-08-17 14:21:59 +0800

    添加demo

--- ZYQRouterDemo/ZYQRouterDemo/ViewController.m
+++ ZYQRouterDemo/ZYQRouterDemo/ViewController.m
@@ -9,6 +9,80 @@
 #import "ViewController.h"
 #import "ZYQRouter.h"
 
+@interface TmpView : UIView
+
+@end
+
+@implementation TmpView
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    if (self=[super initWithFrame:frame]) {
+        self.backgroundColor=[UIColor whiteColor];
+        UIButton *btn=[[UIButton alloc] initWithFrame:self.bounds];
+        [btn setTitle:@"主程序" forState:UIControlStateNormal];
+        [btn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        btn.titleLabel.font=[UIFont systemFontOfSize:30];
+        [self addSubview:btn];
+        
+        [btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return self;
+}
+
+-(void)btnClick{
+    [[self nextResponder] zyq_routerEventWithName:@"MyTap" userInfo:nil];
+}
+
+@end
+
+@interface TmpView1 : UIView
+
+@end
+@implementation TmpView1
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    if (self=[super initWithFrame:frame]) {
+        TmpView *view=[[TmpView alloc] initWithFrame:frame];
+        [self addSubview:view];
+    }
+    return self;
+}
+
+@end
+
+@interface TmpView2 : UIView
+
+@end
+
+@implementation TmpView2
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    if (self=[super initWithFrame:frame]) {
+        TmpView1 *view=[[TmpView1 alloc] initWithFrame:frame];
+        [self addSubview:view];
+    }
+    return self;
+}
+
+@end
+
+@interface TmpView3 : UIView
+
+@end
+
+@implementation TmpView3
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    if (self=[super initWithFrame:frame]) {
+        TmpView2 *view=[[TmpView2 alloc] initWithFrame:frame];
+        [self addSubview:view];
+    }
+    return self;
+}
+
+@end
+
+
 @interface ViewController ()
 
 @end
@@ -18,21 +92,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UIButton *btn=[[UIButton alloc] initWithFrame:self.view.bounds];
-    [btn setTitle:@"主程序" forState:UIControlStateNormal];
-    [btn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    btn.titleLabel.font=[UIFont systemFontOfSize:30];
+    TmpView3 *btn=[[TmpView3 alloc] initWithFrame:self.view.bounds];
     [self.view addSubview:btn];
     
-    [btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 
--(void)btnClick{
-    NSLog(@"点击了");
-    
-    [ZYQRouter openURL:@"module1://view?log=我是主程序"];
+-(void)zyq_routerEventWithName:(NSString *)eventName userInfo:(id)userInfo{
+    if ([eventName isEqualToString:@"MyTap"]) {
+        NSLog(@"点击了");
+        
+        [ZYQRouter openURL:@"module1://view?log=我是主程序"];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
